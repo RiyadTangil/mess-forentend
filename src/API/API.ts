d6338a10a3168ade2456bba0e21cdd0065a91b4a
RiyadTangil <riyadkhanxp2@gmail.com>
2024-02-15 16:24:50 +0600

    done

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,2 +1,2 @@
-export const  rootDomain="http://localhost:5000/api/v1"
-// export const  rootDomain="https://uni-auth-backend-riyad.vercel.app/api/v1"
+// export const  rootDomain="http://localhost:5000/api/v1"
+export const  rootDomain="https://uni-auth-backend-riyad.vercel.app/api/v1"
