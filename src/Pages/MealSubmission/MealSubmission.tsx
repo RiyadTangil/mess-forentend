c78536c9e1152500580be969c08fb11ab98bf690
RiyadTangil <riyadkhanxp2@gmail.com>
2024-02-23 14:48:17 +0600

    done

--- src/Pages/MealSubmission/MealSubmission.tsx
+++ src/Pages/MealSubmission/MealSubmission.tsx
@@ -133,7 +133,7 @@ const MealSubmission: React.FC = () => {
       return `${parts[2]}-${parts[1]}-${parts[0]}`;
     } else {
       // Handle invalid date format
-      return `Invalid Date ${parts.length}`+parts;
+      return `Invalid Date ${parts.length}`+"parts"+parts+"inputDate"+inputDate;
     }
   }
   const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
